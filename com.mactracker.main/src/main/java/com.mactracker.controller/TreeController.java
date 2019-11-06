d02b21db7349cf95b460a4af401071b4cd8a99f3
Timothy Raquet <timothy.raquet@oracle.com>
2019-11-06 17:16:28 -0500

    Fix filter not working

--- com.mactracker.main/src/main/java/com.mactracker.controller/TreeController.java
+++ com.mactracker.main/src/main/java/com.mactracker.controller/TreeController.java
@@ -35,8 +35,8 @@ public class TreeController {
 
             if (query.getFilter() != null && query.getFilter().getStartTime() != null && query.getFilter().getStopTime() != null) {
                 try {
-                    startTime = Integer.parseInt(query.getTimeStart());
-                    stopTime = Integer.parseInt(query.getTimeStop());
+                    startTime = Integer.parseInt(query.getFilter().getStartTime());
+                    stopTime = Integer.parseInt(query.getFilter().getStopTime());
                     filterLocation = query.getFilter().getLocation();
                 } catch (Exception e ) {}
             }
@@ -81,7 +81,6 @@ public class TreeController {
                 treeMap.put(loc, treeMacList.get(loc).size());
             }
 
-            System.out.println(treeMap);
             List<Tree>treeMapReturn = new ArrayList<Tree>();
             for (String loc : treeMap.keySet()) {
                 treeMapReturn.add(new Tree(loc, treeMap.get(loc)));
